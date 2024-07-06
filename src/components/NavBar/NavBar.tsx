779d963327cd325f365f4d385d859a600e81a78b
Mohamedstt <mohamedschof@gmail.com>
2024-07-06 18:48:00 +0100

    Style: Navbar component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,18 +3,33 @@ import { AppBar, MenuItem, styled, Toolbar } from "@mui/material"
 
 
 const NavBar = () => {
-    const StyledToobar = styled(Toolbar)(()=>({
-       display: 'flex',
-       justifyContent: 'Space-evenly',
+    const StyledToobar = styled(Toolbar)(({ theme })=>({
+      display: 'flex',
+      justifyContent: 'space-evenly',
+      alignItems: 'center',
+      backgroundColor: theme.palette.primary.main,
+      borderRadius: '30px',
     }))
+    const StyledMenuItem = styled(MenuItem)<{ active?: boolean }>(({ theme, active }) => ({
+      color: active ? theme.palette.primary.main : theme.palette.primary.contrastText,
+      backgroundColor: active ? 'white' : 'transparent',
+      borderRadius: '20px',
+      padding: theme.spacing(1, 2),
+      fontSize: '1.2rem', 
+      '&:hover': {
+        backgroundColor: theme.palette.primary.light,
+        color: theme.palette.primary.main,
+      },
+    }));
+  
 
     return (
       <>
         <AppBar position="absolute">
             <StyledToobar>
-            <MenuItem>About</MenuItem>
-            <MenuItem>Skills</MenuItem>
-            <MenuItem>Projects</MenuItem>
+            <StyledMenuItem >About</StyledMenuItem>
+            <StyledMenuItem>Skills</StyledMenuItem>
+            <StyledMenuItem>Projects</StyledMenuItem>
             </StyledToobar>
         </AppBar>
       </>
