e8d9d56ca480015e66cc6adc3bf6707222599d38
Mohamedstt <mohamedschof@gmail.com>
2024-07-06 17:28:10 +0100

    feat: created hero

---
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -0,0 +1,67 @@
+import { Box, Container, Grid, styled, Typography } from "@mui/material"
+import Avatar from "../../../../assets/images/avatar.jpg"
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import StyledButton from "../../../../components/StyledButton/StyledButton";
+import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground";
+
+const Hero = () => {
+
+    const StyledHero = styled("div")(({theme})=>({
+        backgroundColor: theme.palette.primary.main,
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+
+    }))
+
+    const StyledImg = styled("img")(({theme})=>({
+        width: "80%",
+        borderRadius:"50%",
+        border: `1px soliud ${theme.palette.primary.contrastText}`
+    }))
+
+    return (
+      <>
+        <StyledHero>
+            <Container maxWidth="lg">            
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={5}>
+                        <Box position="relative" >
+                            <Box position="absolute" width={"150%"} top={-100} right={0}>
+                                <AnimatedBackground/>
+                            </Box>
+                            <Box position="relative">
+                            <StyledImg src={Avatar} alt="avatar" />
+                            </Box>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} md={7}>
+                        <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Mohamed Schuster</Typography>
+                        <Typography color="primary.contrastText" variant="h2" textAlign="center"pb={3}>I'm a software developer</Typography>
+                        <Grid container spacing={3} display="flex" justifyContent="center">
+                            <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                                <StyledButton>
+                                    <FileDownloadIcon/>
+                                    <Typography>DOWNLOAD CV</Typography>
+                                </StyledButton>
+                            </Grid>
+                            <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                                <StyledButton>
+                                    <MailOutlineIcon/>
+                                    <Typography>CONTACT ME</Typography>
+                                </StyledButton>
+                            </Grid>
+                        </Grid>
+                        
+                        
+                    </Grid>
+                </Grid>
+            </Container>
+        </StyledHero>
+      </>
+    )
+  }
+  
+  export default Hero
+  
